6e4522380b7a9f11719d48ca04391b50cdf3aaaf
iNerd71 <inerd71@gmail.com>
2021-03-02 21:58:46 -0800

    Lab 3b upload

---
+++ lab3/lab-3b/javascript.js
@@ -0,0 +1,135 @@
+var map = L.map('map').setView([47.25, -122.44], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYnJhbmRvbmNvdWNoOCIsImEiOiJja2h2NHNpcW4xaXJhMnhtanQ5NHp2ZWppIn0.SLSInhDEagBT3c0do7OQag'
+}).addTo(map);
+
+var drawnItems = L.featureGroup().addTo(map);
+
+var cartoData = L.layerGroup().addTo(map);
+var url = "https://brandoncouch8.carto.com/api/v2/sql";
+var urlGeoJSON = url + "?format=GeoJSON&q=";
+var sqlQuery = "SELECT the_geom, description, name FROM lab_3b_brandon";
+function addPopup(feature, layer) {
+    layer.bindPopup(
+        "<b>" + feature.properties.name + "</b><br>" +
+        feature.properties.description
+    );
+}
+
+fetch(urlGeoJSON + sqlQuery)
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+        L.geoJSON(data, {onEachFeature: addPopup}).addTo(cartoData);
+    });
+
+new L.Control.Draw({
+    draw : {
+        polygon : true,
+        polyline : false,
+        rectangle : false,     // Rectangles disabled
+        circle : false,        // Circles disabled
+        circlemarker : false,  // Circle markers disabled
+        marker: true
+    },
+    edit : {
+        featureGroup: drawnItems
+    }
+}).addTo(map);
+
+
+alert("This map is for recording people's favorite places in Tacoma.\nTo submit a place, use the tools on the left side of the map to add a point or draw a polygon on the map.\nWhen you have placed a point or finished drawing a polygon, enter a title and description for the place in the pop-up that appears and click \"Submit.\"");
+
+
+function createFormPopup() {
+    var popupContent =
+        '<form>' +
+        'Title:<br><input type="text" id="input_title"><br>' +
+        'Description:<br><input type="text" id="input_desc"><br>' +
+        '<input type="button" value="Submit" id="submit">' +
+        '</form>'
+    drawnItems.bindPopup(popupContent).openPopup();
+}
+
+map.addEventListener("draw:created", function(e) {
+    e.layer.addTo(drawnItems);
+    createFormPopup();
+});
+
+function setData(e) {
+
+    if(e.target && e.target.id == "submit") {
+
+        // Get user name and description
+        var enteredTitle = document.getElementById("input_title").value;
+        var enteredDescription = document.getElementById("input_desc").value;
+
+        // For each drawn layer
+        drawnItems.eachLayer(function(layer) {
+
+    			// Create SQL expression to insert layer
+                var drawing = JSON.stringify(layer.toGeoJSON().geometry);
+                var sql =
+                    "INSERT INTO lab_3b_brandon (the_geom, name, description) " +
+                    "VALUES (ST_SetSRID(ST_GeomFromGeoJSON('" +
+                    drawing + "'), 4326), '" +
+                    enteredTitle + "', '" +
+                    enteredDescription + "')";
+                console.log(sql);
+
+                // Send the data
+                fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: "q=" + encodeURI(sql)
+                })
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log("Data saved:", data);
+                })
+                .catch(function(error) {
+                    console.log("Problem saving the data:", error);
+                });
+
+            // Transfer submitted drawing to the CARTO layer
+            //so it persists on the map without you having to refresh the page
+            var newData = layer.toGeoJSON();
+            newData.properties.description = enteredDescription;
+            newData.properties.name = enteredTitle;
+            L.geoJSON(newData, {onEachFeature: addPopup}).addTo(cartoData);
+
+        });
+
+        // Clear drawn items layer
+        drawnItems.closePopup();
+        drawnItems.clearLayers();
+
+    }
+}
+
+document.addEventListener("click", setData);
+
+map.addEventListener("draw:editstart", function(e) {
+    drawnItems.closePopup();
+});
+map.addEventListener("draw:deletestart", function(e) {
+    drawnItems.closePopup();
+});
+map.addEventListener("draw:editstop", function(e) {
+    drawnItems.openPopup();
+});
+map.addEventListener("draw:deletestop", function(e) {
+    if(drawnItems.getLayers().length > 0) {
+        drawnItems.openPopup();
+    }
+});
