a0d4164bbac948e4abe9f411f236df03cc8e0f4d
iNerd71 <inerd71@gmail.com>
2021-03-20 17:54:11 -0700

    Update javascript.js

--- lab3/lab-3c/javascript.js
+++ lab3/lab-3c/javascript.js
@@ -95,28 +95,24 @@ function createFormPopup() {
                 '<legend>Dining options: </legend>' +
                 '<ul>' +
                   '<li>' +
-                    '<input type="hidden" name="dinein" value="false">' +
                     '<label for="dinein">' +
                       '<input type="checkbox" id="dinein" name="dinein" value="true">' +
                       'Dine-in' +
                     '</label>' +
                   '</li>' +
                   '<li>' +
-                    '<input type="hidden" name="drivethru" value="false">' +
                     '<label for="drivethru">' +
                       '<input type="checkbox" id="drivethru" name="drivethru" value="true">' +
                       'Drive-Through' +
                     '</label>' +
                   '</li>' +
                   '<li>' +
-                    '<input type="hidden" name="pickup" value="false">' +
                     '<label for="pickup">' +
                       '<input type="checkbox" id="pickup" name="pickup" value="true">' +
                       'Pick-up' +
                     '</label>' +
                   '</li>' +
                   '<li>' +
-                    '<input type="hidden" name="delivery" value="false">' +
                     '<label for="delivery">' +
                       '<input type="checkbox" id="delivery" name="delivery" value="true">' +
                       'Delivery' +
@@ -162,7 +158,7 @@ function setData(e) {
         var isDineIn = document.getElementById("dinein").checked;
         var isDriveThru = document.getElementById("drivethru").checked;
         var isPickUp = document.getElementById("pickup").checked;
-        var isDelivered = document.getElementById("delivery").checked;
+        var isDelivered = document.getElementById("delivery").check;
         var enteredPhone = document.getElementById("phone").value;
         var enteredWebsite = document.getElementById("website").value;
 
